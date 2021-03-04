a9a4adf1950aec1107971143c2dc2f4295449423
Your Name <arminpp1379@gmail.com>
2021-03-04 23:24:10 +0330

    some debuggins

--- Hudgkin_Huxley_Model.m
+++ Hudgkin_Huxley_Model.m
@@ -22,9 +22,9 @@ beta_m = 4*exp(u/18);
 alpha_h = .07 * exp(u/20);
 beta_h = 1 ./ (1+exp(3 + .1*u));
 %intial condintions
-n_initialCond = 0.29;
+n_initialCond = 0.32;
 m_initialCond = 0.05;
-h_initialCond = 0.55;
+h_initialCond = 0.59;
 dv = 0;
 dn = 0;
 dm = 0;
@@ -37,7 +37,7 @@ h = h_initialCond * ones(1,T);
 
 
 for i = 1:(T-1)
-    dv = (gl*(El - v(i)) - gNa*(m(i)^3)*h(i)*(v(i) - ENa) - gK*(n(i)^4).*(v(i) - EK) + I(i))/Cm;
+    dv = (gl*(El - v(i)) - gNa*(m(i)^3)*h(i)*(v(i) - ENa) - gK*(n(i)^4).*(v(i) - EK) + 200)/Cm;
     v(i+1) = v(i) + dv*dt;
     u = vr - v;
     n(i+1) = n(i) + dn*dt;
@@ -49,15 +49,15 @@ for i = 1:(T-1)
     beta_m = 4*exp(u/18);
     alpha_h = .07 * exp(u/20);
     beta_h = 1 ./ (1+exp(3 + .1*u));
-    dn = (-n(i+1) + alpha_n(i+1)/(alpha_n(i+1) + beta_n(i+1)))*(alpha_n(i+1) + beta_n(i+1));
-    dm = (-m(i+1) + alpha_m(i+1)/(alpha_m(i+1) + beta_m(i+1)))*(alpha_m(i+1) + beta_m(i+1));
-    dh = (-h(i+1) + alpha_h(i+1)/(alpha_h(i+1) + beta_h(i+1)))*(alpha_h(i+1) + beta_h(i+1));
+    dn = (-n(i) + alpha_n(i)/(alpha_n(i) + beta_n(i)))*(alpha_n(i) + beta_n(i));
+    dm = (-m(i) + alpha_m(i)/(alpha_m(i) + beta_m(i)))*(alpha_m(i) + beta_m(i));
+    dh = (-h(i) + alpha_h(i)/(alpha_h(i) + beta_h(i)))*(alpha_h(i) + beta_h(i));
 end
     
-plot(t,v); %Plot v-t
+%plot(t,v); %Plot v-t
 
-%subplot(2,1,1); plot tau-v
-%plot(v,1./(alpha_n + beta_n),v,1./(alpha_m + beta_m),v,1./(alpha_h + beta_h));
-%subplot(2,1,2); plot probabilites_steadystates-v
-%plot(v,alpha_n./(alpha_n + beta_n),v,alpha_m./(alpha_m + beta_m),v,alpha_h./(alpha_h + beta_h));
+subplot(2,1,1); %plot tau-v
+plot(v,1./(alpha_n + beta_n),v,1./(alpha_m + beta_m),v,1./(alpha_h + beta_h));
+subplot(2,1,2); %plot probabilites_steadystates-v
+plot(v,alpha_n./(alpha_n + beta_n),v,alpha_m./(alpha_m + beta_m),v,alpha_h./(alpha_h + beta_h));
 
