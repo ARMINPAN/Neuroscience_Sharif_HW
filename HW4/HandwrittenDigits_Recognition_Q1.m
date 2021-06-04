64eb45c04ab84cb1b6e364a1251e009fa2b69d48
Your Name <arminpp1379@gmail.com>
2021-06-04 12:02:27 +0430

    neural network structure

--- HW4/HandwrittenDigits_Recognition_Q1.m
+++ HW4/HandwrittenDigits_Recognition_Q1.m
@@ -44,14 +44,74 @@ end
 W12 = (rand(25,401)).*2*0.12 - 0.12; % first layer weights
 W23 = (rand(10,26)).*2*0.12 - 0.12; % second layer weights
 
+a1 = [ones(3000,1) train]; % input of first layer
+a2 = sigmoid_calculator(a1*((W12).')); % second layer neurons
+a2 = [ones(3000,1) a2]; % add additional neurons
+a3 = sigmoid_calculator(a2*((W23).')); % third layer neurons
 
 % part 4
 % cost function alogrithm
+% create expected output
+expected_output = zeros(3000,10);
+for i=1:3000
+    if y_train(i) == 10
+        expected_output(i,10) = 1;
+    elseif y_train(i) == 9
+        expected_output(i,9) = 1;
+    elseif y_train(i) == 8
+        expected_output(i,8) = 1;
+    elseif y_train(i) == 7
+        expected_output(i,7) = 1;
+    elseif y_train(i) == 6
+        expected_output(i,6) = 1;
+    elseif y_train(i) == 5
+        expected_output(i,5) = 1;
+    elseif y_train(i) == 4
+        expected_output(i,4) = 1;
+    elseif y_train(i) == 3
+        expected_output(i,3) = 1;
+    elseif y_train(i) == 2
+        expected_output(i,2) = 1;
+    elseif y_train(i) == 1
+        expected_output(i,1) = 1;
+    end
+    
+end
 
- 
+% cost function
+J = cost_function(expected_output,a3,W12,W23);
+
+  
 
 
+% functions
+function J = cost_function(expected_output,a3,W12,W23)
+J = 0;
+landa = 1; % regularization parameter
+for m=1:3000
+    for k=1:10
+J = J +(1/3000)*(((-expected_output(m,k)*log(a3(m,k)))-...
+    ((1-expected_output(m,k))*(1-log(a3(m,k))))));
+    end
+end
+for j=1:25
+    for k=2:401
+        J = J + (landa/(2*3000))*(W12(j,k))^2;
+    end
+end
+for j=1:10
+    for k=2:26
+        J = J + (landa/(2*3000))*(W23(j,k))^2;
+    end
+end
 
+end
 
+function output = sigmoid_calculator(input)
+output = 1./(1+exp(-input));
+end
 
+function output = simgoid_derivative_calculator(input)
+output = exp(-input)./((1+exp(-input)).^2);
+end
 
