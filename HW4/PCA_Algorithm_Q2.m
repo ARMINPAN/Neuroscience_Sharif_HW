5ed98d5d1a0c513a188d807bcdcde97262077258
Your Name <arminpp1379@gmail.com>
2021-06-10 00:31:50 +0430

    PCA for image compressing(eigenfaces and eigen values)

--- HW4/PCA_Algorithm_Q2.m
+++ HW4/PCA_Algorithm_Q2.m
@@ -5,6 +5,7 @@
 clc; clear;
 % part 1
 % load data
+% pca algorithm
 load('1d_pcadata.mat');
 Xmean = mean(X(:,1));
 Ymean = mean(X(:,2));
@@ -24,5 +25,41 @@ grid on;
 axis equal
 plot(finalMaxW(:,1)+Xmean,finalMaxW(:,2)+Ymean,'LineWidth',2);
 plot(finalMinW(:,1)+Xmean,finalMinW(:,2)+Ymean,'LineWidth',2);
-title('principal component analysis ','interpreter','latex');
-%%
+title('principal component analysis','interpreter','latex');
+%% part 2
+clc; clear;
+load('faces.mat');
+meanVector = mean(X,2);
+X = X - meanVector;
+covMatrix = (X.')*X;
+[e landa] = eig(covMatrix);
+principalMaxE = e(:,length(covMatrix));
+% plot 49 top eigenfaces
+figure
+for i=1:49
+        subplot(7,7,i);
+        imshow(mat2gray(reshape(e(:,length(covMatrix)-i+1),[32 32])));
+        sgtitle('49 top eigen vectors')
+end
+% find the coeffs for the weighted sum of 150 eigenfaces for reconstruction
+coefficients = zeros(100,150);
+output = zeros(1024,100);
+% compressed pictures
+figure
+for i=1:100
+    for j=1:150
+        coefficients(i,j) = dot(X(i,:),e(:,1025-j));
+        output(:,i) = output(:,i) + coefficients(i,j).*(e(:,1025-j));
+    end
+    subplot(10,10,i);
+    sgtitle('first 100 compressed images')
+    imshow(mat2gray(reshape(output(:,i),[32 32])));
+end
+% orginal pictures
+figure
+for i=1:100
+    subplot(10,10,i);
+    sgtitle('first 100 original images')
+    imshow(mat2gray(reshape(X(i,:),[32 32])));
+end
+
